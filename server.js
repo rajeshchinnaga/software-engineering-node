0563abbbf943141088ce47d1a0b31ada41f5600f
rajeshchinnaga <39455723+rajeshchinnaga@users.noreply.github.com>
2022-02-07 15:09:03 -0500

    Final Changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/rajesh-db');
+mongoose.connect('mongodb+srv://rajeshchinnaga:Lokesh$2919@fse.xeh0y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 const express = require('express');
 const app = express();
 
 require('./controllers/UserController')(app);
 
 const PORT = 4000;
-app.listen(PORT);
+app.listen(process.env.PORT || PORT);
