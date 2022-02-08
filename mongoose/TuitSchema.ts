90ba3b389b55f832ece2adc49f3e5802b1c2ab12
adarshreddy87 <sparkadarshreddy@gmail.com>
2022-02-08 15:04:11 -0500

    Final changes

--- mongoose/TuitSchema.ts
+++ mongoose/TuitSchema.ts
@@ -2,7 +2,7 @@ import mongoose, {Schema} from "mongoose";
 import Tuit from "../models/Tuit";
 const TuitSchema = new mongoose.Schema<Tuit>({
     tuit: {type: String, required: true},
-    postedBy: {type: String},
+    postedBy: {type: String, ref:'UserModel'},
     postedOn: {type: Date, default: Date.now}
 }, {collection: "tuits"});
 export default TuitSchema;
