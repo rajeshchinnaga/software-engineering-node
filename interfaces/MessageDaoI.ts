0455c9ee5860b105eb4b6344b34e7077adf57628
adarshreddy87 <sparkadarshreddy@gmail.com>
2022-02-25 22:37:20 -0500

    Message changes

--- interfaces/MessageDaoI.ts
+++ interfaces/MessageDaoI.ts
@@ -1,7 +1,7 @@
 import Message from "../models/Message";
 
 export default interface MessageDaoI{
-    userSendsMessage(message : string, uid1:string, uid2: string) : Promise<Message>;
+    userSendsMessage(message : Message, uid1:string, uid2: string) : Promise<any>;
     userDeletesMessage(mid:string) : Promise<any>;
     findAllMessagesSentByUser(uid:string) : Promise<Message[]>;
     findAllMessagesReceivedByUser(uid:string) : Promise<Message[]>;
