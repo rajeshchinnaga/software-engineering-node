3be5e0bd4cb22026b2e30d360980bf2a6aa9073b
adarshreddy87 <sparkadarshreddy@gmail.com>
2022-02-25 21:07:31 -0500

    Changed to global Mongo URI

--- server.ts
+++ server.ts
@@ -26,7 +26,7 @@ var cors = require('cors')
 // connect to the database
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const connectionString = `mongodb+srv://adarsh:Adarsh=97@softwareeng.ueqhd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://adarsh:Adarsh=97@software-engg.8s8gk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 mongoose.connect(
     connectionString
     // "mongodb://localhost:27017/Tuiter"
