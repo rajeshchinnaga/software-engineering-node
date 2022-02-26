c62fca8c8a0551a964bd22077345c3d575bd5e97
adarshreddy87 <sparkadarshreddy@gmail.com>
2022-02-25 20:39:33 -0500

    Implemented Like and Message RESTful Web services

---
+++ daos/LikeDao.ts
@@ -0,0 +1,64 @@
+/**
+ * @file Implements DAO managing data storage of likes. Uses mongoose LikeModel
+ * to integrate with MongoDB
+ */
+import LikeDaoI from "../interfaces/LikeDaoI";
+import LikeModel from "../mongoose/LikeModel";
+import Like from "../models/Like";
+
+/**
+ * @class LikeDao Implements Data Access Object managing data storage of likes
+ * @property {LikeDao} likeDao Private single instance of LikeDao
+ */
+export default class LikeDao implements LikeDaoI {
+    private static likeDao: LikeDao | null = null;
+
+    /**
+     * Creates singleton DAO instance
+     * @returns LikeDao
+     */
+    public static getInstance = (): LikeDao => {
+        if(LikeDao.likeDao === null) {
+            LikeDao.likeDao = new LikeDao();
+        }
+        return LikeDao.likeDao;
+    }
+
+    private constructor() {}
+
+    /**
+     * Uses LikeModel to retrieve all like documents and eventually the users that liked a
+     * particular tuit from the likes collection
+     * @param {string} tid Primary key of the tuit
+     * @returns Promise To be notified when like instances are retrieved from the database
+     */
+    findAllUsersThatLikedTuit = async (tid: string): Promise<Like[]> =>
+        LikeModel.find({tuit: tid}).populate("likedBy").exec();
+
+    /**
+     * Uses LikeModel to retrieve all like documents and eventually the tuits that are liked by a
+     * particular user from the likes collection
+     * @param {string} uid Primary key of the user
+     * @returns Promise To be notified when like instances are retrieved from the database
+     */
+    findAllTuitsLikedByUser = async (uid: string): Promise<Like[]> =>
+        LikeModel.find({likedBy: uid}).populate("tuit").exec();
+
+    /**
+     * Inserts a like instance into the database
+     * @param {string} uid Primary key of the user that likes the tuit
+     * @param {string} tid Primary key of the tuit that is liked by the user
+     * @returns Promise To be notified when like instance is inserted into the database
+     */
+    userLikesTuit = async (uid: string, tid: string): Promise<any> =>
+        LikeModel.create({tuit: tid, likedBy: uid});
+
+    /**
+     * Removes the like instance from the database.
+     * @param {string} uid Primary key of user that unlikes the tuit
+     * @param {string} tid Primary key of tuit being unliked
+     * @returns Promise To be notified when the like instance is removed from the database
+     */
+    userUnlikesTuit = async (uid: string, tid: string): Promise<any> =>
+        LikeModel.deleteOne({tuit: tid, likedBy: uid});
+};
---
+++ interfaces/MessageDaoI.ts
@@ -0,0 +1,9 @@
+import Message from "../models/Message";
+
+export default interface MessageDaoI{
+    userSendsMessage(message : Message, uid1:string, uid2: string) : Promise<Message>;
+    userDeletesMessage(mid:string) : Promise<any>;
+    findAllMessagesSentByUser(uid:string) : Promise<Message[]>;
+    findAllMessagesReceivedByUser(uid:string) : Promise<Message[]>;
+    findAllMessages() : Promise<Message[]>;
+};
